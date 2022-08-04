4f81a7d967f3fc2257715a3c58fe75f8b5e4b3fb
Dennis Brink <57118725+dennisbr3003@users.noreply.github.com>
2022-08-05 00:25:47 +0200

    Added (nested) exponent calculation + minor fixes

--- app/src/main/java/com/dennis_brink/android/mycalculator/MainActivity.java
+++ app/src/main/java/com/dennis_brink/android/mycalculator/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,
            btnDel, btnMem, btnAc, btnMr,
-           btnBracketOpen, btnBracketClose, btnDev, btnPlus, btnMin, btnMulti, btnEquals, btnInvert, btnDot;
+           btnBracketOpen, btnBracketClose, btnDev, btnPlus, btnMin, btnMulti, btnEquals, btnInvert, btnDot, btnExponent;
 
     TextView tvHistory, tvResult, tvMemory, tvLabelMemory;
 
@@ -69,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         btnEquals = findViewById(R.id.btnEquals);
         btnInvert = findViewById(R.id.btnInvert);
         btnDot = findViewById(R.id.btnDot);
+        btnExponent = findViewById(R.id.btnExp);
 
         tvHistory = findViewById(R.id.textViewHistory);
         tvResult = findViewById(R.id.textViewResult);
@@ -299,8 +300,11 @@ public class MainActivity extends AppCompatActivity {
                 if (hasError()) {return;}
 
                 String result="";
-                tvHistory.setText(String.format("%s%s", tvHistory.getText().toString(), tvResult.getText().toString()));
-                result=evaluateExpression(tvHistory.getText().toString());
+
+                String expression = checkForExponents(tvResult.getText().toString());
+
+                tvHistory.setText(String.format("%s%s", tvHistory.getText().toString(), expression));
+                result = evaluateExpression(tvHistory.getText().toString());
 
                 Log.d("DENNIS_B", "btnEquals.onClick: result evaluation (before split): " + result);
 
@@ -308,7 +312,11 @@ public class MainActivity extends AppCompatActivity {
 
                 tvResult.setText(div_result[0]); // the first element is always there
                 if(div_result.length > 1) { // only in case of error
-                    tvMemory.setText(div_result[1]);
+                    try {
+                        tvMemory.setText(div_result[1] + div_result[2]);
+                    } catch(Exception e){
+                        tvMemory.setText(div_result[1]);
+                    }
                     tvLabelMemory.setText(R.string._dtl);
                 }
 
@@ -356,6 +364,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnExponent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (hasError()) {return;}
+
+                isProduct=false;
+                tvResult.setText(String.format("%s%s", tvResult.getText().toString(), "^"));
+            }
+        });
     }
 
     private void numberClick(String view){
@@ -384,7 +402,9 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Log.d("DENNIS_B", "operatorClick: bracket count (open and closed) = " + bracketopen + "-" + bracketclosed );
             if(bracketclosed == bracketopen) {
-                tvHistory.setText(String.format("%s%s%s", tvHistory.getText().toString(), tvResult.getText().toString(), view));
+                /* check for exponents */
+                String expression = checkForExponents(tvResult.getText().toString());
+                tvHistory.setText(String.format("%s%s%s", tvHistory.getText().toString(), expression, view));
                 tvResult.setText(R.string._zero);
                 Log.d("DENNIS_B", "operatorClick: setting number = null");
                 number = null;
@@ -424,9 +444,67 @@ public class MainActivity extends AppCompatActivity {
         if (tvResult.getText().toString().equals(getString(R.string._error))) {
             tvResult.setTextColor(Color.RED);
             tvMemory.setTextColor(Color.RED);
+            tvMemory.setTextColor(Color.RED);
             return true;
         }
         return false;
+    }
+
+    private String checkForExponents(String expression){
+
+        Log.d("DENNIS_B", "checkForExponents: check expression for exponents " + expression);
+
+        String expression_fixed="";
+
+        if(expression.contains("^")) {
+
+            String[] div_result = expression.split("\\^");
+            Log.d("DENNIS_B", "checkForExponents: number of expression sections: " + div_result.length);
+
+            for (int i = 0; i < div_result.length; i++) {
+                try {
+                    Log.d("DENNIS_B", "checkForExponents: array content : index " + i + ", value " + div_result[i]);
+                    if(i==0) {
+                        Log.d("DENNIS_B", "checkForExponents: exponent part 2 (next array element, i = 0): index " + (i + 1) + " value " + div_result[i + 1]);
+                        expression_fixed = String.format("Math.pow(%s,%s)", div_result[i], div_result[i + 1]);
+                        Log.d("DENNIS_B", "checkForExponents: result expression: " + expression_fixed);
+                    }else{
+                        Log.d("DENNIS_B", "checkForExponents: exponent is nested. Previous exponent used from index : " + (i - 1) + " value " + div_result[i - 1]);
+                        expression_fixed = String.format("Math.pow(%s,%s)", div_result[i - 1], div_result[i]);
+                        Log.d("DENNIS_B", "checkForExponents: result expression: " + expression_fixed);
+                    }
+                    Log.d("DENNIS_B", "checkForExponents: length array and value of i: " + div_result.length + " " + (i + 1));
+                    if(div_result.length <= i){
+                        return expression_fixed;
+                    } else {
+                          if(i==0) {
+                              div_result[i + 1] = expression_fixed;
+                          } else{
+                              div_result[i] = expression_fixed;
+                          }
+                    }
+                    /*
+                    input: (2+2)^2^2^2
+                    length array and value of  i: 4 4
+                    fixed expression in div_result[0]: (2+2)
+                    fixed expression in div_result[1]: Math.pow((2+2),2)
+                    fixed expression in div_result[2]: Math.pow(Math.pow((2+2),2),2)
+                    fixed expression in div_result[3]: Math.pow(Math.pow(Math.pow((2+2),2),2),2)
+                    fixed expression: length=4; index=4
+                    expression Math.pow(Math.pow(Math.pow((2+2),2),2),2)
+                     */
+                    if(i==0) { // only skip one element here
+                        i = i + 1;
+                    }
+                } catch (Exception e) {
+                    Log.d("DENNIS_B", "checkForExponents. Error occurred. Returning fixed expression. Error: " + e.getMessage());
+                    return div_result[div_result.length - 1];
+                }
+            }
+            Log.d("DENNIS_B", "checkForExponents.returning fixed expression: " + div_result[div_result.length - 1]);
+            return div_result[div_result.length - 1];
+        }
+        return expression;
     }
 
     private String evaluateExpression(String expression){
